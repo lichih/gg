ad55769e0c8bec8371e081e24313217391b25473
Li-chih Wu <lichihwu@gmail.com>
2023-12-22 13:46:55 +0800

    .

--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -3,8 +3,28 @@ namespace tests;
 [TestClass]
 public class UnitTest1
 {
+    string base_url = "http://localhost:4649";
     [TestMethod]
-    public void TestMethod1()
+    public void TestGetMessage()
     {
+        Uri uri = new Uri(base_url + "/Message");
+        var client = new HttpClient();
+        var response = client.GetAsync(uri).Result;
+        var result = response.Content.ReadAsStringAsync().Result;
+        Console.WriteLine(result);
     }
+    [TestMethod]
+    public void TestPostMessage()
+    {
+        Uri uri = new Uri(base_url + "/Message");
+        var client = new HttpClient();
+        var query_string = new Dictionary<string, string>();
+        query_string.Add("sender", "unitest");
+        query_string.Add("content", "unitest content");
+        uri = new Uri(uri.ToString() + "?" + string.Join("&", query_string.Select(x => x.Key + "=" + x.Value)));
+        var response = client.PostAsync(uri, null).Result;
+        var result = response.Content.ReadAsStringAsync().Result;
+        Console.WriteLine(result);
+    }
+
 }
