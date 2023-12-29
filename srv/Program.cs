60332acca7fa93afa146cdf3e34f9b87a957b5f8
Li-chih Wu <lichihwu@gmail.com>
2023-12-29 14:24:31 +0800

    Add WebSocket server and services

--- srv/Program.cs
+++ srv/Program.cs
@@ -1,3 +1,5 @@
+using System.Net.WebSockets;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://*:4649");
 
@@ -9,18 +11,35 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseWebSockets();
+app.Map("/echo", async conext => {
+    Console.WriteLine("on echo");
+    if(conext.WebSockets.IsWebSocketRequest){
+        using var ws = await conext.WebSockets.AcceptWebSocketAsync();
+        // echo back anything we receive
+        while(true){
+            var buffer = new byte[1024 * 4];
+            var result = await ws.ReceiveAsync(buffer, CancellationToken.None);
+            if(result.MessageType == WebSocketMessageType.Close){
+                await ws.CloseAsync(WebSocketCloseStatus.NormalClosure, string.Empty, CancellationToken.None);
+                break;
+            }
+            await ws.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+        }
+    }else{
+        conext.Response.StatusCode = 400;
+    }
+});
+
+await app.RunAsync();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+// app.UseHttpsRedirection();
+// app.UseAuthorization();
+// app.MapControllers();
+// app.Run();
---
+++ wssrv/WsSrv.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+
+namespace hinata;
+
+[Serializable]
+public record Message(string type, string data);
+
+public class EchoYamlSrv : WebSocketBehavior
+{
+    protected override void OnOpen()
+    {
+        var msg = new Message("N", $"opened connection: {ID}, convering YAML to JSON");
+        Send(JsonConvert.SerializeObject(msg));
+        // Send(msg);
+    }
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        var deserializer = new DeserializerBuilder()
+            .WithNamingConvention(CamelCaseNamingConvention.Instance)
+            .Build();
+        try {
+            var data = deserializer.Deserialize<Dictionary<string, string>>(e.Data);
+            var msg = $"pasrsed as YAML: [{e.Data}][{e.Data.Length}]";
+            Send(msg);
+            var json = JsonConvert.SerializeObject(data);
+            msg = $"converted to JSON({json.Length}): {json}";
+            Send(msg);
+        }
+        catch (Exception ex) {
+            var msg = $"failed to parse as YAML: [{e.Data}][{e.Data.Length}] ex: {ex.Message}";
+            Send(msg);
+        }
+    }
+}
+
+public class EchoSrv : WebSocketBehavior
+{
+    protected override void OnMessage(MessageEventArgs e)
+    {
+        var msg = $"received data: [{e.Data}][{e.Data.Length}]";
+        Send(msg);
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var wssv = new WebSocketServer("ws://localhost:4649");
+        wssv.AddWebSocketService<EchoSrv>("/echo");
+        wssv.AddWebSocketService<EchoYamlSrv>("/echo/yaml");
+        wssv.Start();
+        Console.WriteLine("Hinata is listening on port 4649, and providing WebSocket services:");
+        Console.ReadKey(true);
+        wssv.Stop();
+    }    
+}
